2b68fec68f56d37e00ed7c34606a4c7d30f029d4
Joseph Yin <jsphyin@cs.utexas.edu>
2016-05-09 15:53:05 -0500

    Working on emulator

--- src/emulator/emulator.c
+++ src/emulator/emulator.c
@@ -20,17 +20,12 @@ int main(int argc, const char **argv) {
     }
 
     Memory* memory = newMemory();
-    uint64_t e = load(fd,memory);
-    uint64_t pc = read64(memory,e);
-    uint64_t r2 = read64(memory,e+8);*/
-    /*printf("entry = %lx\n",e);
-    printf("pc = %lx\n",pc);
-    printf("r2 = %lx\n",r2);*/
-
-    /* State* s = newState(memory);
+    uint32_t e = load(fd,memory);
+    uint32_t pc = read32(memory,e)
+    
+    State* s = newState(memory);
     s->pc = pc;
-    s->gprs[2] = r2;
-    s->gprs[1] = 0x7eadbeef;
+    s->gprs[13] = 0x7eadbeef;
     s->lr = 0;
     run(s);
     return 0;
